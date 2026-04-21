bc34def86eb709324db927c5e8762030f403a9e8
Jordi <jordiybanyez@gmail.com>
2026-04-21 11:55:33 +0200

    Opti

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    resolveExtensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
 };
 
 export default nextConfig;
